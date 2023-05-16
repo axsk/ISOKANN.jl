3e79c9f68fabb621999cd7b8be87b64ff4b612c8
Sikorski <sikorski@zib.de>
2023-05-16 11:09:59 +0200

    update deps, add reactionpath example, add isosimple.jl

---
+++ src/isosimple.jl
@@ -0,0 +1,111 @@
+# a simple rewrite of the the top layer api
+# functionally equivalent (up to the plots, loggers, ...) to isomolly.jl
+
+using ISOKANN
+using ProgressMeter
+using ISOKANN: koopman, shiftscale, learnstep!
+Tensor = Array{<:Any, 3}
+
+"""
+bare bones ISOKANN, for educational purposes
+model is a Flux neural network, opt an Optimiser,
+data a tuple of (x,y) coordinates, n the number of iterations
+"""
+function isokann_simple(model, opt, data, n)
+    xs::Matrix, ys::Tensor = data                # decompose data in start and endpoints
+    for _ in 1:n
+        ks       = mean(model(ys), dims=3)[1,:]  # Monte-Carlo estimate of Kx
+        target   = shiftscale(ks)                # SKx
+        learnstep!(model, xs, target, opt)       # Neural Network update
+    end
+    return model                                 # the learned χ-function
+end
+
+"""
+ISOKANN with advanced data handling,
+dataset handles possible resampling of data
+"""
+function isokann(model, opt, dataset, n)
+    losses = Float64[]
+    @showprogress for i in 1:n
+        data = getdata!(dataset, model)
+        loss = isostep!(model, opt, data)
+        push!(losses, loss)
+    end
+    losses
+end
+
+function isostep!(model, opt, data::Tuple{<:Matrix, <:Tensor})
+    xs, ys = data
+    ks     = koopman(model, ys)  # Monte-Carlo estimate of Kx
+    target = shiftscale(ks)      # SKx
+    loss   = learnstep!(model, xs, target, opt)  # Neural Network update
+    return loss
+end
+
+" basic dataset type for fixed data "
+struct StaticData
+    data
+end
+getdata!(d::StaticData, model) = d.data
+
+"dataset type handling chi-stratified subsampling and traindata-subsampling"
+mutable struct ChiStratData
+    data
+    subdata
+    iter
+    i_resample
+    n_resample
+    i_generate
+    n_generate
+    sim
+end
+
+function getdata!(d::ChiStratData, model)
+    i = d.iter += 1
+    if (i%d.i_generate == 0)  # sample new data
+        d.data = ISOKANN.adddata(d.data, model, d.sim, d.n_generate)
+    end
+    if (d.i_resample) > 0 && (i%d.i_resample == 0) # subsample data
+        d.subdata = ISOKANN.datasubsample(model, d.data, n_resample)
+    end
+    return d.subdata
+end
+
+"wrap the old ISORun into the simplified isokann() call"
+function isokann(iso::ISOKANN.IsoRun)
+    i=iso
+    data = ChiStratData(
+        iso.data,
+        ISOKANN.datasubsample(iso.model, iso.data, iso.nx),
+        0,
+        i.np * i.nl,
+        i.nx,
+        i.nl*i.np*i.nres,
+        i.ny,
+        i.sim)
+
+    isokann(iso.model, iso.opt, data, i.nd*i.np*i.nl)
+end
+
+
+
+
+
+
+
+
+
+
+
+"a tuple which allows mutation and creation of new fields"
+mutable struct MutTuple
+    tuple::NamedTuple
+end
+MutTuple(;kwargs...) = MutTuple((;kwargs...))
+tuple(m::MutTuple) = getfield(m, :tuple)
+Base.getproperty(m::MutTuple, x::Symbol) = getfield(tuple(m), x)
+function Base.setproperty!(m::MutTuple, x::Symbol, v)
+    t = merge(tuple(m), NamedTuple{(x,)}(v))
+    setfield!(m, :tuple, t)
+end
--- src/reactionpath.jl
+++ src/reactionpath.jl
@@ -4,25 +4,82 @@
 using Zygote
 using OrdinaryDiffEq
 
-function force(sim, x, chi, direction=1, orth=0.01)
+function reactionforce(sim, x, chi, direction=1, orth=0.01)
     # setcoords
     sys = setcoords(sim.sys, x)
-    fs = Molly.forces(sys, find_neighbors(sys))
-    fs = ustrip(reduce(vcat, fs))
-    fc = Zygote.gradient(x->first(chi(x)), x)[1]
+    f_sys = Molly.accelerations(sys, find_neighbors(sys))  # TODO: scale with gamma?
+    f_sys = ustrip(reduce(vcat, f_sys))
 
-    fso = fs - fc * dot(fs, fc) / dot(fc, fc)
-    dot(fso, fc)
+    #@show norm(f_sys)
 
-    f = -fc #+ fso * 0.001
-    f = direction * fc / norm(fc)^2 + fso * orth
+    f_chi = Zygote.gradient(x->first(chi(x)), x)[1]
+    magn = norm(f_chi)
+    f_chi =  normalize(direction * f_chi)
+
+    ratio = dot(f_sys, f_chi)
+    f_orth = (f_sys - f_chi * ratio)
+
+    #@show norm(f_chi)
+    #@show norm.([f_sys, f_orth])
+    #@show dot(f_orth, f_chi)
+
+    f = f_chi / magn + orth * f_orth
+    #f_orth = f_sys - f_chi * dot(f_sys, f_chi) / dot(f_chi, f_chi)
+
+    #f = -fc #+ fso * 0.001
+    #f = direction * fc / norm(fc)^2 + fso * orth
+
+    #@show norm(f)
     collect(f)
-
 end
 
-function transitionpath(sim, x0, chi, orth = 0.01; solver=Rosenbrock23(), kwargs...)
+
+function transitionpath(sim, x0, chi; orth = 0.01, solver=Rosenbrock23(), tmax = 1, kwargs...)
     direction = (first(chi(x0))) > 0.5 ? -1 : 1
-    prob = ODEProblem((x,p,t)->force(sim, x, chi, direction, orth), x0, (0,1))
+    prob = ODEProblem((x,p,t)->velocity(sim, x, chi, direction, orth), x0, (0,tmax))
     sol = solve(prob, solver; kwargs...)
     #return reduce(hcat, sol.u)
+end
+
+export reactionpath
+function reactionpath(sim, x0, chi; extrapolate=0.01, orth=0.01, solver=Euler(), kwargs...)
+
+    t0 = chi(x0) |> first
+    @show t0
+
+    bw = solve(ODEProblem((x,p,t)->reactionforce(sim, x, chi, -1, orth), x0, (0-extrapolate, t0)), solver; kwargs...)
+    #@show bw.stats
+
+    fw = solve(ODEProblem((x,p,t)->reactionforce(sim, x, chi, 1, orth), x0, (t0, 1+extrapolate)), solver; kwargs...)
+    #@show fw.stats
+
+    fw = reduce(hcat, fw.u)
+    bw = reduce(hcat, bw.u)
+
+    u = hcat(bw[:, end:-1:1], fw)
+end
+
+using SpecialFunctions: erf
+
+# mean of the folded normal distribution
+# https://en.wikipedia.org/wiki/Folded_normal_distribution
+function folded_normal_mean(mu, sigma)
+    sigma * sqrt(2/pi) * exp(-mu^2/(2*sigma^2)) + mu * erf(mu/sqrt(2*sigma^2))
+end
+
+""" return the diagonal of the noise factor sigma of the brownian dynamics """
+function sigma(sys::Molly.System, temp, gamma = 1/u"s")
+    k = Unitful.k
+    mass = Molly.masses(sys)
+    σ = sqrt.(k * temp ./ mass *  gamma)
+    repeat(σ, inner=3)
+end
+
+""" what is the mean speed for a given force """
+function mean_speed(velocity, sys, temp)
+    mu = norm(velocity)
+    v = velocity ./ mu
+    sig = sigma(sys, temp)
+    sig = norm(sig.*v)
+    return folded_normal_mean(mu, sigma)
 end
