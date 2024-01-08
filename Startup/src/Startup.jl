722148fa65691770c9519bddc48df8758fc76a74
Sikorski <sikorski@zib.de>
2024-01-08 16:53:56 +0100

    remove Startup module

--- Startup/src/Startup.jl
+++
@@ -1,7 +0,0 @@
-module Startup
-
-#using Revise, OhMyREPL, BenchmarkTools
-
-using CUDA, ChainRulesCore, ClusterManagers, Dictionaries, FLoops, Flux, ForwardDiff, JLD2, LinearAlgebra, LsqFit, Lux, Molly, NNlib, NNlibCUDA, Optimisers, Parameters, Plots, ProgressMeter, Random, SlurmClusterManager, SnoopPrecompile, SplitApplyCombine, StaticArrays, StatsBase, StochasticDiffEq, Unitful, Zygote
-
-end # module Startup
