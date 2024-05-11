86740e1d3676f618c797334c2f9abfe4388ee373
Sikorski <sikorski@zib.de>
2024-05-11 16:32:13 +0200

    openmm: cleaner atom selection. minor reorganization

--- src/simulators/openmm.jl
+++ src/simulators/openmm.jl
@@ -1,16 +1,33 @@
 module OpenMM
 
 using PyCall, CUDA
+using LinearAlgebra: norm
 
+import JLD2
 import ISOKANN: ISOKANN, IsoSimulation,
     propagate, dim, randx0,
     featurizer, defaultmodel,
     savecoords, getcoords, force, pdb,
     force, potential, lagtime
 
-import JLD2
-
 export OpenMMSimulation, FORCE_AMBER, FORCE_AMBER_IMPLICIT
+
+DEFAULT_PDB = "$(@__DIR__)/../../data/systems/alanine dipeptide.pdb"
+FORCE_AMBER = ["amber14-all.xml"]
+FORCE_AMBER_IMPLICIT = ["amber14-all.xml", "implicit/obc2.xml"]
+
+function __init__()
+    # install / load OpenMM
+    try
+        pyimport_conda("openmm", "openmm", "conda-forge")
+        pyimport_conda("openmmforcefields", "openmmforcefields", "conda-forge")
+        pyimport_conda("joblib", "joblib")
+
+        @pyinclude("$(@__DIR__)/mopenmm.py")
+    catch
+        @warn "Could not load openmm."
+    end
+end
 
 ###
 
@@ -29,51 +46,6 @@ struct OpenMMSimulation <: IsoSimulation
     mmthreads::Union{Int,String}
     momenta::Bool
 end
-
-function Base.show(io::IO, mime::MIME"text/plain", sim::OpenMMSimulation)#
-    println(
-        io, """
-        OpenMMSimulation(;
-            pdb="$(sim.pdb)",
-            ligand="$(sim.ligand)",
-            forcefields=$(sim.forcefields),
-            temp=$(sim.temp),
-            friction=$(sim.friction),
-            step=$(sim.step),
-            steps=$(sim.steps),
-            features=$(sim.features))"""
-    )
-end
-
-""" generate `n` random inintial points for the simulation `mm` """
-function randx0(sim::OpenMMSimulation, n)
-    x0 = stack([getcoords(sim)])
-    return reshape(propagate(sim, x0, n), :, n)
-end
-
-function dim(sim::OpenMMSimulation)
-    return sim.pysim.system.getNumParticles() * 3
-end
-
-function featurizer(sim::OpenMMSimulation)
-    if sim.features isa (Vector{Int})
-        ix = vec([1, 2, 3] .+ ((sim.features .- 1) * 3)')
-        return ISOKANN.pairdistfeatures(ix)
-    elseif sim.features isa (Vector{Tuple{Int,Int}}) # local pairwise distances
-        inds = sim.features
-        return coords -> ISOKANN.pdists(coords, inds)
-    elseif sim.features == :all
-        return ISOKANN.flatpairdists
-    else
-        error("unknown featurizer")
-    end
-end
-
-function defaultmodel(sim::OpenMMSimulation; nout, kwargs...)
-    ISOKANN.pairnet(sim; nout, kwargs...)
-end
-
-DEFAULT_PDB = "$(@__DIR__)/../../data/systems/alanine dipeptide.pdb"
 
 """
     OpenMMSimulation(; pdb=DEFAULT_PDB, ligand="", forcefields=["amber14-all.xml", "amber14/tip3pfb.xml"], temp=298, friction=1, step=0.002, steps=100, features=nothing, minimize=false)
@@ -122,20 +94,50 @@ function OpenMMSimulation(;
     pysim = @pycall py"defaultsystem"(pdb, ligand, forcefields, temp, friction, step, minimize; addwater, padding, ionicstrength, forcefield_kwargs)::PyObject
     if features isa Number
         radius = features
-        features = calphas_and_spheres(pdb, pysim, radius)
-        features = remove_atom_pairs(pysim, "H", features)
+        features = [calpha_pairs(pysim); local_atom_pairs(pysim, radius)]
     end
     return OpenMMSimulation(pysim::PyObject, pdb, ligand, forcefields, temp, friction, step, steps, features, nthreads, mmthreads, momenta)
 end
 
-localpdistinds(sim::OpenMMSimulation, radius) = localpdistinds(sim.pysim, radius)
-localpdistinds(pysim::PyObject, radius) = ISOKANN.localpdistinds(getcoords(pysim, false), radius)
+function Base.show(io::IO, mime::MIME"text/plain", sim::OpenMMSimulation)#
+    println(
+        io, """
+        OpenMMSimulation(;
+            pdb="$(sim.pdb)",
+            ligand="$(sim.ligand)",
+            forcefields=$(sim.forcefields),
+            temp=$(sim.temp),
+            friction=$(sim.friction),
+            step=$(sim.step),
+            steps=$(sim.steps),
+            features=$(sim.features))"""
+    )
+end
 
+function featurizer(sim::OpenMMSimulation)
+    if sim.features isa (Vector{Int})
+        ix = vec([1, 2, 3] .+ ((sim.features .- 1) * 3)')
+        return ISOKANN.pairdistfeatures(ix)
+    elseif sim.features isa (Vector{Tuple{Int,Int}}) # local pairwise distances
+        inds = sim.features
+        return coords -> ISOKANN.pdists(coords, inds)
+    elseif sim.features == :all
+        return ISOKANN.flatpairdists
+    else
+        error("unknown featurizer")
+    end
+end
 
-FORCE_AMBER = ["amber14-all.xml"]
-#FORCE_AMBER_EXPLICIT = ["amber14-all.xml", "amber14/tip3pfb.xml"]
-FORCE_AMBER_IMPLICIT = ["amber14-all.xml", "implicit/obc2.xml"]
+""" generate `n` random inintial points for the simulation `mm` """
+function randx0(sim::OpenMMSimulation, n)
+    x0 = hcat(getcoords(sim))
+    xs = propagate(sim, x0, n)
+    return dropdims(xs, dims=3)
+end
 
+lagtime(sim::OpenMMSimulation) = sim.step * sim.steps
+dim(sim::OpenMMSimulation) = return length(getcoords(sim))
+defaultmodel(sim::OpenMMSimulation; kwargs...) = ISOKANN.pairnet(sim; kwargs...)
 pdb(s::OpenMMSimulation) = s.pdb
 
 """
@@ -208,10 +210,6 @@ function getcoords(sim::PyObject, momenta)
     end
 end
 
-function getvelocities(sim::PyObject)
-    py"$sim.context.getState(getVelocities=True).getVelocities(asNumpy=True).value_in_unit(nanometer/picosecond).flatten()"
-end
-
 function setcoords(sim::PyObject, coords::AbstractVector{T}, momenta) where {T}
     t = reinterpret(Tuple{T,T,T}, Array(coords))
     if momenta
@@ -224,6 +222,9 @@ function setcoords(sim::PyObject, coords::AbstractVector{T}, momenta) where {T}
     end
 end
 
+force(sim::OpenMMSimulation, x::CuArray) = force(sim, Array(x)) |> cu
+potential(sim::OpenMMSimulation, x::CuArray) = potential(sim, Array(x))
+
 function force(sim::OpenMMSimulation, x)
     CUDA.reclaim()
     sim = sim.pysim
@@ -231,9 +232,6 @@ function force(sim::OpenMMSimulation, x)
     f = sim.context.getState(getForces=true).getForces(asNumpy=true)
     f = f.value_in_unit(f.unit) |> permutedims |> vec
 end
-
-force(sim::OpenMMSimulation, x::CuArray) = force(sim, Array(x)) |> cu
-potential(sim::OpenMMSimulation, x::CuArray) = potential(sim, Array(x))
 
 function potential(sim::OpenMMSimulation, x)
     CUDA.reclaim()
@@ -243,22 +241,9 @@ function potential(sim::OpenMMSimulation, x)
     v = v.value_in_unit(v.unit)
 end
 
-lagtime(sim::OpenMMSimulation) = sim.step * sim.steps
 
 ### PYTHON CODE
 
-function __init__()
-    # install / load OpenMM
-    try
-        pyimport_conda("openmm", "openmm", "conda-forge")
-        pyimport_conda("openmmforcefields", "openmmforcefields", "conda-forge")
-        pyimport_conda("joblib", "joblib")
-
-        @pyinclude("$(@__DIR__)/mopenmm.py")
-    catch
-        @warn "Could not load openmm."
-    end
-end
 
 function savecoords(path, sim::OpenMMSimulation, coords::AbstractArray{T}) where {T}
     coords = ISOKANN.cpu(coords)
@@ -274,65 +259,37 @@ function savecoords(path, sim::OpenMMSimulation, coords::AbstractArray{T}) where
     file.close()
 end
 
+### FEATURE FILTERS
 
-### TESTS
-
-function test_getsetcoords(sim)
-    x = getcoords(sim)
-    bak = copy(x)
-    setcoords(sim, x)
-    @assert getcoords(sim) == bak
+function atomfilter(atom)
+    !(
+        atom.element.symbol == "H" ||
+        atom.residue.name == "HOH"
+    )
 end
 
-function test_openmm(replicas=2)
-    sim = OpenMMSimulation()
-    x0 = stack([getcoords(sim) for _ in 1:replicas])
-    propagate(sim, x0, 3, nthreads=2)
-end
+function local_atom_pairs(pysim::PyObject, radius; atomfilter=atomfilter)
+    coords = reshape(getcoords(pysim, false), 3, :)
+    atoms = filter(atomfilter, pysim.topology.atoms() |> collect)
+    inds = map(atom -> atom.index + 1, atoms)
 
-function calphas(pdbfile)
-    inds = Int[]
-    for l in readlines(pdbfile)
-        s = split(l)
-        length(s) < 3 && continue
-        s[3] == "CA" && push!(inds, parse(Int, s[2]))
+    pairs = Tuple{Int,Int}[]
+    for i in 1:length(inds)
+        for j in i+1:length(inds)
+            if norm(coords[:, i] - coords[:, j]) <= radius
+                push!(pairs, (inds[i], inds[j]))
+            end
+        end
     end
-    return inds
-end
-
-function calphas_and_spheres(pdbfile::String, pysim::PyObject, radius)
-    cind = calphas(pdbfile)
-    cpairs = [(x, y) for x in cind, y in cind][ISOKANN.halfinds(length(cind))]
-    rpairs = localpdistinds(pysim, radius)
-    return unique([rpairs; cpairs])
-end
-
-calphas_and_spheres(sim::OpenMMSimulation, radius) = calphas_and_spheres(sim.pdb, sim.pysim, radius)
-
-function remove_atom_pairs(pysim::PyObject, symbol, pairs)
-    symbols = [a.element.symbol for a in pysim.topology.atoms()]
-    inds = findall(symbols .== symbol)
-    pairs = filter(p -> !(p[1] in inds || p[2] in inds), pairs)
     return pairs
 end
 
-function filteratoms(pdbfile, pred)
-    inds = Int[]
-    for l in readlines(pdbfile)
-        s = split(l)
-        length(s) < 3 && continue
-        s[1] == "ATOM" || continue
-        pred(s[3]) && push!(inds, parse(Int, s[2]))
-    end
-    return inds
+function calpha_pairs(pysim::PyObject)
+    local_atom_pairs(pysim, Inf; atomfilter=x -> x.name == "CA")
 end
 
 
-function residue_atoms(sim::OpenMMSimulation)
-    [res.name => [parse(Int, a.id) for a in res.atoms()] for res in sim.pysim.topology.residues()]
-end
-
-noHatoms(pdbfile) = filteratoms(pdbfile, !contains("H"))
+### SERIALIZATION
 
 struct OpenMMSimulationSerialized
     pdb
