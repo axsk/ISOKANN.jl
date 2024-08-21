93e97c20938501940a3ae9eee5475893236fbeb8
Sikorski <sikorski@zib.de>
2024-08-21 15:41:40 +0200

    remove IsoMu and VGV example code from the code base

--- test/runtests.jl
+++ test/runtests.jl
@@ -45,34 +45,6 @@ end
         end
     end
 
-    @testset "IsoMu.jl" begin
-        using ISOKANN.IsoMu
-        datapath = "/data/numerik/ag_cmd/trajectory_transfers_for_isokann/data/8EF5_Aspargine_116_100ns_7.4_v2"
-        if !ispath(datapath)
-            println("could not access IsoMu datapath $datapath")
-            return @test_broken false
-        end
-        iter = 10
-
-        data = DataLink(datapath, startpos=2)
-        mu = isokann(data, gpu=false)
-        train!(mu, iter)
-        out = Base.Filesystem.tempname() * ".pdb"
-        save_reactive_path(mu, sigma=0.05, out=out)
-
-        @test true
-    end
-
-    @testset "vgv" begin
-        datapath = ISOKANN.VGV_DATA_DIR
-        if !ispath(datapath)
-            println("could not access VGV datapath $datapath")
-            return @test_broken false
-        end
-        v = ISOKANN.VGVData(datapath, nx=10, nk=2)
-        ISOKANN.vgv_examplerun(v, Base.Filesystem.tempname())
-        @test true
-    end
 
     @testset "Iso2 and IsoSimulation operations" begin
         iso = Iso2(OpenMMSimulation(), nx=10)
