5006fa92b7b3c05c110fb22b2efafed4e02c928d
Sikorski <sikorski@zib.de>
2024-10-22 17:44:28 +0200

    update scripts

--- scripts/waterimpexp.jl
+++ scripts/waterimpexp.jl
@@ -2,37 +2,40 @@ using ISOKANN
 using Plots
 
 
-VAL_NX = 10#00
-VAL_STEPS = 100#00
-VAL_NK = 10
+VAL_NX = 1000 # number of validation samples
+VAL_STEPS = 10_000 # steps between validation samples
+VAL_NK = 10 # validation burst size
 
-NX = 100#00
-NITER = 100#00
+NX = 100_000 # number of training samples
+NITER = 10_000 # number of training iterations
 
 REVERSE = true # also count reverse transitions
 PDB = "data/systems/alanine dipeptide.pdb"
 
-features = collect(1:22)
+features = collect(1:22) # use pairwise dists between all 22 atoms
 sims = [
-    OpenMMSimulation(forcefields=OpenMM.FORCE_AMBER; features),
-    OpenMMSimulation(forcefields=OpenMM.FORCE_AMBER_IMPLICIT; features),
-    OpenMMSimulation(addwater=true, padding=10; features)]
+    OpenMMSimulation(forcefields=OpenMM.FORCE_AMBER; features), # vacuum
+    OpenMMSimulation(forcefields=OpenMM.FORCE_AMBER_IMPLICIT; features), # implicit water
+    OpenMMSimulation(addwater=true, padding=1; features)] # explicit water
 
 
-
-valsim = OpenMMSimulation(addwater=true, padding=10; features)
+@info "computing validation data"
+valsim = OpenMMSimulation(addwater=true, padding=1; features)
 valtraj = laggedtrajectory(valsim, VAL_NX, steps=VAL_STEPS)
 valdata = SimulationData(valsim, valtraj, VAL_NK) |> gpu
 
-for (i, sim) in enumerate(sims)
+isos = map(enumerate(sims)) do (i, sim)
+    @info "generating data $i"
     xs = laggedtrajectory(sim, NX)
-    #scatter_ramachandran(xs) |> display
+    scatter_ramachandran(xs) |> display
+    savefig("water rama $i.png")
     data = SimulationData(sim, data_from_trajectory(xs; reverse=REVERSE))
 
     vallog = ISOKANN.ValidationLossLogger(data=valdata)
-    iso = Iso(data, loggers=[vallog])
+    @info "training model $i"
+    iso = Iso(data, loggers=Any[vallog], autoplot=30)
 
     run!(iso, NITER)
     plot_training(iso)
-    savefig("$i.png")
+    savefig("water trained $i.png")
 end
--- scripts/worm.jl
+++ scripts/worm.jl
@@ -1,13 +1,15 @@
 using ISOKANN
+using ISOKANN.OpenMM
 using LinearAlgebra
 using StatsBase
 
 pdb = "data/chris/af2_multimer_pdbfixer copy.pdb"
 ligand = "data/chris/rigid_docking_af2multimer_1.sdf"
 temp = 300
+forcefield_kwargs = Dict("nonbondedCutoff" => 3)
+simargs = (; pdb, ligand, temp, forcefield_kwargs)
 
-sim = OpenMMSimulation(; pdb, ligand, temp);
-OpenMM.savecoords("test.pdb", sim);
+sim = OpenMMSimulation(; simargs...);
 
 chainid = [a.residue.chain.index for a in sim.pysim.topology.atoms()]
 m = OpenMM.masses(sim)
@@ -16,20 +18,71 @@ forcemask = chainid .== 1
 
 centerofmass = sum(x .* m', dims=2) ./ sum(m)
 
-
 centerofligand = let x = x[:, forcemask], m = m[forcemask]
     sum(x .* m', dims=2) ./ sum(m)
 end
 
-force = normalize!(centerofligand .- centerofmass) |> vec
-strength = 10
+outward = normalize!(centerofligand .- centerofmass) |> vec
 
-bias(q; kwargs...) = (force .* forcemask' .* strength) |> vec
+STRENGTH::Float64 = 1
+PUSH::Float64 = 0.1
+
+function constacc(q; F, kwargs...)
+    PUSH .* vec(outward .* forcemask' .* m')
+end
+
+function adaptivebias(q; F, kwargs...)
+    meanforce = mean((reshape(F, 3, :)./m')[:, forcemask], dims=2)
+    d = dot(outward, meanforce)
+
+    mflig = meanforce = mean((reshape(F, 3, :)./m')[:, .!forcemask], dims=2)
+    bilig = vec(-1 .* dot(outward, mflig) .* outward .* .!forcemask' .* m')
+    #if d < 0
+    #    d = 0
+    #end
+    bias = vec(-1 .* d .* outward .* forcemask' .* m')
+    (bias .+ bilig) .* STRENGTH
+end
+
+function combinedbias(q; F, kwargs...)
+    adaptivebias(q; F, kwargs...) .+ constacc(q; F, kwargs...)
+end
+
+function simbiased(; bias=combinedbias, strength=1.0, push=0.3, nx=1000, saveevery=10)
+    global STRENGTH = strength
+    global PUSH = push
+    sim = OpenMMSimulation(; simargs..., bias=combinedbias, minimize=true, step=0.002)
+    traj = laggedtrajectory(sim, nx)
+    path = "worms $(string(sim.bias)) s$(strength) p$(push).pdb"
+    savecoords(path, sim, traj.values[:, 1:saveevery:end])
+    println(path)
+    return traj.values
+end
 
 
-sim = OpenMMSimulation(; pdb, ligand, temp, bias)
-OpenMM.minimize!(sim)
 
-traj = laggedtrajectory(sim, 10)
-@show mean_and_std(traj.weights)
-savecoords("worm.pdb", sim, traj.values)
+function protocol()
+    # obtained "pushout" trajectory by biased sampling
+    xs0 = simbiased(push=5, strength=0, nx=30, saveevery=1)
+
+    sim = OpenMMSimulation(; simargs...)
+
+    f1 = FeaturesPairs(sim; maxdist=1, atomfilter=a -> a.residue.chain.id == "X", maxfeatures=100)
+    f2 = FeaturesPairs(sim; maxdist=1, atomfilter=a -> a.name == "CA", maxfeatures=100)
+
+    a = atoms(sim)
+    ca = [i for (i, a) in enumerate(a) if a.name == "CA"]
+    lig = [i for (i, a) in enumerate(a) if a.residue.chain.index == 1]
+
+    cross = StatsBase.sample(([(i, j) for i in ca, j in lig]), 1000, replace=false)
+
+    featurizer = FeaturesPairs(vcat(f1.pairs, f2.pairs, cross))
+
+
+    prop = propagate(sim, xs0, 10) |> ISOKANN.flattenlast
+
+    data = SimulationData(sim, prop, 3; featurizer)
+
+    # instantiate simulationdata and iso object with 3 koopman sample
+    iso = Iso(data, autoplot=3)
+end
