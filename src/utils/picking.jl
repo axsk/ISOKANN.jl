4d7cced1bf8049325d8418d7cfbec455053e2767
Sikorski <sikorski@zib.de>
2025-01-24 15:09:13 +0100

    picking: add new picking implementation. also supports rmsd aligned picking through picking_aligned

---
+++ src/utils/picking.jl
@@ -0,0 +1,46 @@
+using CUDA: CuArray
+using StatsBase: mean
+using NNlib: batched_mul, batched_transpose
+using ProgressMeter
+using LinearAlgebra: svd
+using ISOKANN: ISOKANN
+
+
+function picking(X, n; dists = pairwise_one_to_many)
+    @assert size(X, 2) >= n
+
+    d = similar(X, size(X, 2), n) .= 0
+    mins = similar(X, size(X, 2)) .= Inf
+    origin = similar(X, size(X, 1)) .= 0
+    qs = Int[]
+    q = argmax(dists(origin, X))  # start with the point furthest from origin
+
+    @showprogress 1 "Picking..." for i in 1:n
+        push!(qs, q)
+        d[:, i] .= dists(X[:, q], X)
+        mins .= min.(mins, d[:, i])
+        q = argmax(mins)
+    end
+
+    return X[:, qs], qs, d
+end
+
+function picking_aligned(x::AbstractMatrix, m::Integer)
+    n = size(x, 2)
+    x = copy(x)
+    x = reshape(x, 3, :, n)
+    x .-= mean(x, dims=2)
+    x = reshape(x, :, n)
+
+    _, qs, d = picking(x, m, dists=ISOKANN.batched_kabsch_rmsd)
+    return x[:, qs], qs, d
+end
+
+
+using Distances: SqEuclidean, pairwise
+# no CUDA support yet
+pairwise_one_to_many(x, xs) = pairwise(SqEuclidean(), xs, reshape(x, :, 1), dims=2) |> vec
+
+
+#
+
