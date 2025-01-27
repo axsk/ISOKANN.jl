21c903132a3790bbc432da969951602be2845d7c
Sikorski <sikorski@zib.de>
2025-01-27 18:03:12 +0100

    add interactive reactioncoordinate plot through `serve`
    rename visualize

--- src/makie.jl
+++ src/makie.jl
@@ -383,3 +383,65 @@ function voxelize(data, n)
     end
     return x ./ maximum(x)
 end
+
+function scatter_rc(rc, chi; markersize=5, kwargs...)
+    fig = Figure()
+    ax = Axis3(fig[1, 1])
+    ms = Observable(ones(size(rc, 2)) .* markersize)
+    fig, ax, plt =
+    #plt= 
+        WGLMakie.scatter(rc, color=chi;
+            colormap=:thermal,
+            markersize=ms,
+            strokewidth=0, 
+            kwargs...
+        )
+    #d=DataInspector(ax.scene)
+
+    i = Observable(0)
+
+    function hover(inspector, plt, index)
+        #@show plt, index
+        i[] = index
+        return true
+    end
+
+    #plt.inspector_hover = hover
+
+    on(events(fig).mousebutton, priority=2) do event
+        if event.button == Mouse.left && event.action == Mouse.press
+            plt, ind = pick(fig)
+            if ind > 0
+                i[] > 0 && (ms[][i[]] = markersize)
+                ms[][ind] = 20
+                notify(ms)
+                i[] = ind
+            end
+        end
+    end
+
+
+    Makie.Camera3D(ax.scene, projectiontype=Makie.Orthographic)
+    fig, ax, plt, i
+end
+
+
+""" visualize(iso, reactioncoords, chi)
+
+Scatter plot of the reaction coordinates `reactioncoords` with `chi` as color.
+Clicking on any of the data points will visualize the corresponding molecular structure in a separate figure.
+"""
+function visualize(iso, reactioncoords, chi)
+    fig, ax, plt, i = scatter_rc(reactioncoords, chi)
+    c = lift(i) do i
+        if i > 0
+            ISOKANN.centercoords(iso.data.coords[1][:, i])
+        else
+            zeros(size(iso.data.coords[1], 1))
+        end
+    end
+    fig2 = ISOKANN.plotmol(c, iso.data.sim.pysim, showbonds=false)
+    App() do s
+        DOM.div([fig, fig2])
+    end
+end
